25b688c8f45ab23a4e216a1b8dff197dc1afce01
Loren Schlomer <me@schlomie.com>
2018-03-29 14:14:37 -0700

    Vue app is working now.  generated passwords get copied to clipboard

--- src/js/sgplus-web.js
+++ src/js/sgplus-web.js
@@ -1,20 +1,17 @@
 import _ from 'underscore';
 import 'bootstrap/dist/js/bootstrap.bundle.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../css/styles.css';
 import Vue from 'vue/dist/vue.common.js';
-import VueClipboard from 'vue-clipboard2'
-import $ from 'jquery';
+import uuid from 'uuid/v4'
 
 const sgp = require('supergenpass-lib');
+window.$ = window.jQuery = require('jquery');
 
 let methods = ['md5', 'sha512'];
 let booleans = [true, false];
 
 
 $(() => {
-
-    Vue.use(VueClipboard);
 
     let app = new Vue({
         el: '#sgplus-app',
@@ -28,31 +25,84 @@ $(() => {
             method: 'sha512',
             length: 24,
             rounds: 10,
-
             subdomains: false,
 
-            clearSeconds: 10,
-            clearEntries: 20
+            generatedPassword: null,
+            generationError: null
         },
 
         methods: {
-            copyToClipboard: function() {
+            generatePassword: function() {
+
+                if (_.isEmpty(this.password) || _.isEmpty(this.url)) {
+                    return; // We cannot generate without one of these
+                }
+
+                let rounds = parseInt(this.rounds, 10);
+                let length = parseInt(this.length, 10);
+
                 let opts = {
-                    secret: this.secret,
+                    secret: (this.secret || ''),
                     method: this.method,
-                    length: this.length,
-                    hashRounds: this.rounds,
-                    remoeSubdomains: true === this.subdomains
+                    length: (length || ''),
+                    hashRounds: (rounds || 10),
+                    removeSubdomains: true === this.subdomains
                 };
 
-                sgp.generate(this.password, this.url, opts, (pw) => {
+                try {
 
-                    this.$copyText(pw).then(() => {
-                        console.log('copied!');
-                    })
+                    this.generationError = null;
 
-                    console.log('PW: %s', pw);
-                });
+                    sgp.generate(this.password, this.url, opts, (pw) => {
+                        this.generatedPassword = pw;
+                    });
+
+                } catch (err) {
+                    this.generationError = err.message;
+                }
+            },
+
+            copy: function() {
+                if (_.isEmpty(this.generatedPassword)) {
+                    return alert('No password has been generated yet.');
+                }
+
+                this.doCopy();
+            },
+
+            reset: function() {
+                this.url = null;
+                this.secret = null;
+                this.password = null;
+                this.method = 'sha512';
+                this.length = 24;
+                this.rounds = 10;
+                this.subdomains = false;
+
+                this.clearClipboard();
+            },
+
+            clearClipboard: function() {
+                this.generatedPassword = uuid();
+                this.doCopy();
+                this.generatedPassword = null;
+            },
+
+            doCopy: function() {
+
+                if (!_.isEmpty(this.generationError)) {
+                    return alert(this.generationError);
+                }
+
+                let input = $('<input>').val(this.generatedPassword);
+                $('body').append(input);
+                input.select();
+
+                if (!document.execCommand('copy')) {
+                    alert('Failed to copy to clipboard!');
+                }
+
+                input.remove();
             }
         }
     });
